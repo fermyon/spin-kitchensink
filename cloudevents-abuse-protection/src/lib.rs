4ba5fff4945951dab1b4f1915b66b37a828dd462
Jiaxiao Zhou <jiazho@microsoft.com>
2022-05-01 21:41:37 -0700

    fixed the headers
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- cloudevents-abuse-protection/src/lib.rs
+++ cloudevents-abuse-protection/src/lib.rs
@@ -1,17 +1,16 @@
 use anyhow::Result;
+use cloudevents::{AttributesReader, Event};
 use spin_sdk::{
     http::{Request, Response},
     http_component,
 };
-use cloudevents::{Event, AttributesReader};
-
 
 /// A spin component that can be used to test the event-grid-validation.
 /// For more info, see here: https://github.com/cloudevents/spec/blob/v1.0/http-webhook.md#42-validation-response
 #[http_component]
 fn validation(req: Request) -> Result<Response> {
     if req.method() == http::Method::OPTIONS {
-        println!("received validation request");   
+        println!("received validation request");
         let mut origin: &str = "";
         let mut callback: &str = "";
         let rate = "120";
@@ -22,29 +21,34 @@ fn validation(req: Request) -> Result<Response> {
             if k == "webhook-request-callback" {
                 callback = v.to_str().unwrap();
             }
-        }); 
+        });
         println!("callback: {}", callback);
         let req = http::Request::builder()
             .method("GET")
+            .header("webhook-allowed-origin", origin)
+            .header("webhook-allowed-rate", rate)
             .uri(callback)
             .body(None);
-        
+
         let req = req.map_err(|err| {
             eprintln!("failed to build request: {:?}", err);
             anyhow::anyhow!("failed to build request")
         })?;
 
         println!("making request: {:?}", req);
-        
+
         let res = spin_sdk::outbound_http::send_request(req);
         let mut res = res.map_err(|err| {
             eprintln!("failed to send request: {:?}", err);
             anyhow::anyhow!("failed to send request")
         })?;
-        
-        res.headers_mut().insert("webhook-response-origin", origin.try_into()?);
-        res.headers_mut().insert("webhook-response-rate", rate.try_into()?);
-        res.headers_mut().insert("spin-component", "rust-outbound-http".try_into()?);
+
+        res.headers_mut()
+            .insert("webhook-allowed-origin", origin.try_into()?);
+        res.headers_mut()
+            .insert("webhook-allowed-rate", rate.try_into()?);
+        res.headers_mut()
+            .insert("spin-component", "rust-outbound-http".try_into()?);
         Ok(res)
     } else {
         println!("received event");
@@ -54,13 +58,9 @@ fn validation(req: Request) -> Result<Response> {
             println!("event source: {}", event.source());
             println!("event id: {}", event.id());
             println!("event type: {}", event.ty());
-            Ok(http::Response::builder()
-            .status(200)
-            .body(None)?)
+            Ok(http::Response::builder().status(200).body(None)?)
         } else {
-            Ok(http::Response::builder()
-            .status(400)
-            .body(None)?)
+            Ok(http::Response::builder().status(400).body(None)?)
         }
-    } 
+    }
 }
